5befb55920f56d75be08a9e1049b3778e29b9cf1
Bruno de Assis Ferreira <33126607+brunodassisf@users.noreply.github.com>
2023-07-16 19:28:09 -0300

    style - change wrapperLayout

--- src/components/WrapperLayout/index.tsx
+++ src/components/WrapperLayout/index.tsx
@@ -4,7 +4,7 @@ function WrapperLayiout({ children }: { children: React.ReactNode }) {
   return (
     <>
       <Header />
-      <main className="container mx-auto pt-10">{children}</main>
+      <main className="container mx-auto pt-10 px-4 xl:px-0">{children}</main>
     </>
   );
 }
