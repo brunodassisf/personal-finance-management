9fc2da5d6c6e083a6b4eba42a1e198d05f7a0ba6
Bruno de Assis Ferreira <33126607+brunodassisf@users.noreply.github.com>
2023-07-16 16:40:04 -0300

    Refactor - add Balance componento to the root page

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,3 +1,14 @@
+import Balance from 'components/Balance';
+
 export default function HomeScreen() {
-  return <div>Home</div>;
+  const data = {
+    name: 'Fulano de tal',
+    phone: 99999999999,
+    balance: 0
+  };
+  return (
+    <div className="grid grid-cols-3">
+      <Balance info={data} />
+    </div>
+  );
 }
