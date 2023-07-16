e720fed20f48561b42d2c0e40313d61d43256b76
Bruno de Assis Ferreira <33126607+brunodassisf@users.noreply.github.com>
2023-07-16 19:27:29 -0300

    create - route user

---
+++ src/app/api/user/route.ts
@@ -0,0 +1,13 @@
+import { NextResponse } from 'next/server';
+
+export async function GET() {
+  try {
+    const response = await fetch(`${process.env.NEXT_PUBLIC_URL}/api/conect`, { method: 'GET' });
+
+    const data = await response.json();
+
+    return NextResponse.json(data);
+  } catch (error) {
+    return NextResponse.json({ error: true, msg: 'Ocorreu um erro, tente novamente mais tarde.' });
+  }
+}
