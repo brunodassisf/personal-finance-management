238a03ae2b32484a7d87d9df379937bcd9f1ddb5
Bruno de Assis Ferreira <33126607+brunodassisf@users.noreply.github.com>
2023-07-16 19:27:45 -0300

    create - file error root

---
+++ src/app/error.tsx
@@ -0,0 +1,14 @@
+'use client';
+
+function Error({ error, reset }: { error: Error; reset: () => void }) {
+  return (
+    <div className="min-h-screen bg-gray-100 flex flex-col justify-center py-12 sm:px-6 lg:px-8">
+      <div className="sm:mx-auto sm:w-full sm:max-w-md">
+        <h2 className="mt-6 text-center text-3xl font-extrabold text-gray-900">Error</h2>
+        <p className="mt-2 text-center text-sm text-gray-600">{error.message}</p>
+      </div>
+    </div>
+  );
+}
+
+export default Error;
