a18ba12fc363a299b8b68c2c38c9ea77168b2c54
Bruno de Assis Ferreira <33126607+brunodassisf@users.noreply.github.com>
2023-07-17 02:08:46 -0300

    Refactor - Balance component format currency and phone number

--- src/components/Balance/index.tsx
+++ src/components/Balance/index.tsx
@@ -1,4 +1,6 @@
 import { FaUser } from 'react-icons/fa';
+import Skeleton from 'react-loading-skeleton';
+import { formatCurrency, formatPhone } from 'util/format';
 
 interface BalanceProps {
   info: {
@@ -9,6 +11,9 @@ interface BalanceProps {
 }
 
 function Balance({ info: { name, phone, balance } }: BalanceProps) {
+  const phoneMasker = formatPhone(phone) || <Skeleton width={100} />;
+  const balanceMasker = formatCurrency(balance) || <Skeleton width={100} />;
+
   return (
     <div className="flex flex-col gap-4">
       <div className="bg-white shadow-md p-4 rounded flex items-center gap-4">
@@ -17,12 +22,12 @@ function Balance({ info: { name, phone, balance } }: BalanceProps) {
         </div>
         <div>
           <h6 className="text-xl font-bold text-emerald-800">{name}</h6>
-          <p className="text-xl font-bold text-gray-400">{phone}</p>
+          <p className="text-xl font-bold text-gray-400">{phoneMasker}</p>
         </div>
       </div>
       <div className="bg-white shadow-md p-4 rounded flex justify-around">
         <span className="text-xl font-bold text-emerald-800">Saldo</span>
-        <span className="text-xl font-bold text-emerald-600">R$ {balance}</span>
+        <span className="text-xl font-bold text-emerald-600">{balanceMasker}</span>
       </div>
     </div>
   );
